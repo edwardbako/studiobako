c57a6ed676213ca8ff93968badb8de2407fa8609
Edward Bako <edward.bako@gmail.com>
2024-06-04 18:40:11 +0700

    Initial commit & basic layout

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,18 @@
+class WelcomeController < ApplicationController
+  before_action { sleep 5 }
+
+  def index
+  end
+
+  def about
+  end
+
+  def contacts
+  end
+
+  def projects
+  end
+
+  def products
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "welcome#index"
+
+  get "projects" => "welcome#projects"
+  get "products" => "welcome#products"
+  get "about" => "welcome#about"
+  get "contacts" => "welcome#contacts"
+
+end

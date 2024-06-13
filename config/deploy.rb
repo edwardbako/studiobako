f30b81bb68beb07f6eed4a6959cbc237862b6f0e
Edward Bako <edward.bako@gmail.com>
2024-06-13 21:14:27 +0700

    Disable capistrano deploy redundant puma:restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,5 +58,5 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-after 'deploy:published', 'puma:restart'
+# after 'deploy:published', 'puma:restart'
 after 'deploy:published', 'seed'
